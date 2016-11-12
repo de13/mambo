97e1173453222aa78d26111110b331b3c3c160ab
Stephane Beuret <stephane.beuret@data-essential.com>
2016-11-12 10:05:40 +0100

    mambo project Dockerfile

---
+++ config_local.py
@@ -0,0 +1,8 @@
+CSRF_SESSION_KEY = 'SuperSecret1'
+SECRET_KEY = 'SuperSecret2'
+SECURITY_PASSWORD_SALT = 'SuperSecret3'
+SERVER_MODE = False
+LOG_FILE = '/var/log/pgadmin4/pgadmin4.log'
+SQLITE_PATH = '/var/lib/pgadmin4/pgadmin4.db'
+SESSION_DB_PATH = '/var/lib/pgadmin4/sessions'
+STORAGE_DIR = '/var/lib/pgadmin4/storage'
